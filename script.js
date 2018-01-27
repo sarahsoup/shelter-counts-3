289cfcc57c8d6d54bda8554643da3da08ea8b43d
sarahsoup <campbell.sar@husky.neu.edu>
2018-01-27 10:34:04 -0500

    language changed to animals

--- script.js
+++ script.js
@@ -243,7 +243,7 @@ d3.csv(link, parse, function(err,counts){
   ratioText
     .append('tspan')
     .attr('class','ratio-text')
-    .text(`dogs and cats that enter a shelter, `);
+    .text(`animals that enter a shelter, `);
   // ratioText
   //   .append('tspan')
   //   .attr('class','ratio-text')
@@ -263,7 +263,7 @@ d3.csv(link, parse, function(err,counts){
   ratioText
     .append('tspan')
     .attr('class','ratio-text')
-    .text(`shelter dogs and cats that will have a live outcome, and `);
+    .text(`shelter animals that will have a live outcome, and `);
   ratioText
     .append('tspan')
     .attr('class','ratio-text')
